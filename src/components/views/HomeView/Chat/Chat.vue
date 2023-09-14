b1f3af0d09a0bd759befb0319b8644693497832a
JorgeRM7 <jorge.rincon@grupo-ortiz.com>
2023-09-14 15:11:05 -0600

    first commit

---
+++ src/components/views/HomeView/Chat/Chat.vue
@@ -0,0 +1,152 @@
+<script setup lang="ts">
+import type { Ref } from "vue";
+// import{ ref } from "vue";
+
+
+
+import { computed, provide, ref } from "vue";
+import useStore from "@src/store/store";
+
+import ChatBottom from "@src/components/views/HomeView/Chat/ChatBottom/ChatBottom.vue";
+import ChatMiddle from "@src/components/views/HomeView/Chat/ChatMiddle/ChatMiddle.vue";
+import ChatTop from "@src/components/views/HomeView/Chat/ChatTop/ChatTop.vue";
+
+const store = useStore();
+
+// search the selected conversation using activeConversationId.
+const activeConversation = computed(() => {
+  let activeConversation = store.conversations.find(
+    (conversation) => conversation.id === store.activeConversationId
+  );
+
+  if (activeConversation) {
+    return activeConversation;
+  } else {
+    return store.archivedConversations.find(
+      (conversation) => conversation.id === store.activeConversationId
+    );
+  }
+});
+
+// provide the active conversation to all children.
+provide("activeConversation", activeConversation.value);
+
+// determines whether select mode is enabled.
+const selectMode = ref(false);
+
+// determines whether all the messages are selected or not.
+const selectAll = ref(false);
+
+// holds the selected conversations.
+const selectedMessages: Ref<number[]> = ref([]);
+
+// (event) add message to select messages.
+const handleSelectMessage = (messageId: number) => {
+  selectedMessages.value.push(messageId);
+
+  if (
+    activeConversation.value &&
+    selectedMessages.value.length === activeConversation.value.messages.length
+  ) {
+    selectAll.value = true;
+  }
+
+  if (!selectMode.value) {
+    selectMode.value = true;
+  }
+};
+
+// (event) remove message from select messages.
+const handleDeselectMessage = (messageId: number) => {
+  selectAll.value = false;
+  selectedMessages.value = selectedMessages.value.filter(
+    (item) => item !== messageId
+  );
+
+  if (activeConversation.value && selectedMessages.value.length === 0) {
+    selectMode.value = false;
+  }
+};
+
+// (event) select all messages.
+const handleSelectAll = () => {
+  if (activeConversation.value) {
+    const messages = activeConversation.value.messages.map(
+      (message) => message.id
+    );
+    selectedMessages.value = messages;
+    selectAll.value = true;
+  }
+};
+
+// (event) remove the selected messages.
+const handleDeselectAll = () => {
+  selectAll.value = false;
+  selectedMessages.value = [];
+};
+
+// (event handle close Select)
+const handleCloseSelect = () => {
+  selectMode.value = false
+  selectAll.value = false;
+  selectedMessages.value = [];
+};
+
+
+let mensajes = ref(
+  []
+);
+
+interface Mensajes {
+  id: number;
+  content: string;
+  sender: "usuario" | "asistente";
+}
+
+const enviarMensaje = (): Mensajes[] => {
+  const messages: Mensajes[] = [
+    {
+      id: 1,
+      content: "Hola, ¿cómo estás?",
+      sender: "usuario",
+    },
+    {
+      id: 2,
+      content: "¡Hola! Estoy bien, gracias.",
+      sender: "asistente",
+    },
+    {
+      id: 3,
+      content: "¿En qué puedo ayudarte hoy?",
+      sender: "asistente",
+    },
+    // Agrega más mensajes según sea necesario
+  ];
+
+  console.log(messages);
+  
+  // mensajes.value.push(texto); 
+  //mensajes=[...mensajes, texto];
+};
+</script>
+
+<template>
+  <div v-if="activeConversation" class="h-full flex flex-col scrollbar-hidden">
+    <ChatTop
+      :select-all="selectAll"
+      :select-mode="selectMode"
+      :handle-select-all="handleSelectAll"
+      :handle-deselect-all="handleDeselectAll"
+      :handle-close-select="handleCloseSelect"
+    />
+    <ChatMiddle
+      :selected-messages="selectedMessages"
+      :handle-select-message="handleSelectMessage"
+      :handle-deselect-message="handleDeselectMessage"
+      :mensajes="mensajes"
+    />
+    <ChatBottom 
+      :send="enviarMensaje"
+    />
+  </div>
+</template>
